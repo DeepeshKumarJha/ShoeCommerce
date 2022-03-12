60a233052634a94cae5eb1527df767a7b9e3a893
Deepesh Jha <76602335+DeepeshKumarJha@users.noreply.github.com>
2022-03-12 14:06:44 +0530

    removed all unused variables

--- src/Products/Product/Product.js
+++ src/Products/Product/Product.js
@@ -2,13 +2,11 @@ import React from "react";
 import "./Product.css";
 
 const Product = (props) => {
-	const shoesSize = document.querySelector(".shoes-size");
-
 	return (
 		<>
 			<div className='product-container'>
 				<div className='product-image-section'>
-					<img src={props.product.image} />
+					<img src={props.product.image} alt={product.name} />
 				</div>
 				<h3>{props.product.name}</h3>
 				<p>{props.product.about}</p>
